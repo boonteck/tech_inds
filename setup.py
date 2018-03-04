d1bad64dfeb2563f997cd4076961ae7a8a5bc2fb
boonteck <bt.boonteck@gmail.com>
2018-03-04 23:28:33 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tech_inds',
+      version='0.1',
+      description='Finance Technical Indicators optimized with Numba',
+      url='https://github.com/boonteck/tech_inds',
+      author='BoonTeck',
+      author_email='bt.boonteck@gmail.com',
+      packages=['tech_inds'],
+      zip_safe=False)
